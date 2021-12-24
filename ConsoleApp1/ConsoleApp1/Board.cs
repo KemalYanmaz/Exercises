930a621113356c88817542ed664dae4ba781b394
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-24 23:36:01 +0300

    instructions added.

--- ConsoleApp1/ConsoleApp1/Board.cs
+++ ConsoleApp1/ConsoleApp1/Board.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     public class Board
     {
         private char[,] _area;
-
+        private int _counter = 0;
         public Board(int width, int height)
         {
             _area = new char[width, height];
@@ -54,28 +54,18 @@ namespace ConsoleApp1
 
         public void Show()
         {
-            /*
-             * for (int i = 0; i < _area.GetLength(0); i++)
-            {
-                Console.WriteLine();
-                for (int j = 0; j < _area.GetLength(1); j++)
-                {
-                    Console.Write($"\t{_area[i, j]}");
-                }
-            }*/
-            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------------------------");
             Console.WriteLine();
             for (int i = _area.GetLength(1)-1; i >= 0 ; i--)
             {
-                Console.WriteLine();
                 for (int j = 0; j < _area.GetLength(0); j++)
                 {
                     Console.Write($"\t{_area[j, i]}");
-                    //Console.WriteLine(i+","+j);
                 }
+                Console.WriteLine();
             }
+            Console.WriteLine($"----------------------------------------------{++_counter}");
             Console.WriteLine();
-            Console.WriteLine("------------------------------");
 
         }
     }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,14 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Lütfen oyun tahtası uzunluklarını arasında boşluk bırakarak giriniz.(Örnek \"5 5\")");
             string[] inputs = Console.ReadLine().Split(' ');
             int width = Convert.ToInt16(inputs[0]);
             int height = Convert.ToInt16(inputs[1]);
-
-            inputs = Console.ReadLine().Split(' ');
-            int x = Convert.ToInt16(inputs[0]);
-            int y = Convert.ToInt16(inputs[1]);
+            int x = 0;
+            int y = 0;
+            do
+            {
+                Console.WriteLine("Lütfen başlangıç konumunu ve yönünü seçiniz. Yönler: 'N'orth, 'E'ast, 'S'outh, 'W'est\n(Örnek \"2 2 N\")");
+                inputs = Console.ReadLine().Split(' ');
+                x = Convert.ToInt16(inputs[0]);
+                y = Convert.ToInt16(inputs[1]);
+            } while (x >= width || y >= height);
             Direction direction;
             switch (inputs[2])
             {
@@ -38,7 +43,11 @@ namespace ConsoleApp1
                     break;
             }
             Game game = new Game(width, height, x, y, direction);
-            while (true) {
+            game.Showboard();
+            Console.WriteLine("Dönüşler: 'L'eft, 'R'ight | İleri Hareket: 'M'");
+            Console.WriteLine("Örnek giriş: RMMLLMMRMLLLMM");
+            while (true)
+            {
                 string input = Console.ReadLine();
                 for (int i = 0; i < input.Length; i++)
                 {
